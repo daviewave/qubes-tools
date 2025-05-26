96ce0ecf92e021030ea1dade658b3dd76f79eac2
ds <dav.silveira@proton.me>
2025-05-26 08:23:13 -0400

    updated envs sourcing file

--- qb-v2/kali-rpm-templates/helper-tools/t/envs.sh
+++ qb-v2/kali-rpm-templates/helper-tools/t/envs.sh
@@ -6,7 +6,12 @@ export qbp=/opt/qubes-builderv2 		# qbp=qubes builder path
 export qbpht=${qbp}/helper-tools                # qbpht=setup tools helper tools
 export st=/opt/t-setup-tools                   	# st=setup tools
 export stht=${st}/helper-tools                  # stht=setup tools helper tools
-export archive_keyring=/usr/share/keyrings
 
-export q_master_key_id=427F11FD0FAA4B080123F01CDDFA1A3E36879494
-#export 
+export qmsk_fp=/usr/share/qubes/qubes-master-key.asc
+export qmsk_id=427F11FD0FAA4B080123F01CDDFA1A3E36879494
+export qrsk=720415900AB8C804
+export qdsk=ED65462EC8D5E4C5
+
+export archive_keyring=/usr/share/keyrings
+export srcs_lists=/etc/apt/sources.list.d
+export trusted_gpgs=/etc/apt/trusted.gpgs.d
