e8ce7dfebc9a1ce34926c8222769ea36d258b16d
ds <dav.silveira@proton.me>
2025-05-26 00:11:04 -0400

    added and tested a script that uses a different gpg fetch 'proxychains4' that doesnt require a search id along with what to put in the .list file (didnt want to spend time figuring out the permissions to automate)

---
+++ qb-v2/kali-rpm-templates/helper-tools/t/04_add_kali_list.sh
@@ -0,0 +1,21 @@
+ #!/bin/bash
+
+./check_env.sh
+if [ $? -eq 1 ]; then
+  exit 1
+fi
+
+echo "(4/8) adding kali gpg key and repo source...."
+
+#1,
+sudo cp "${qbp}/config/proxychains.conf" /etc
+
+#2,
+sudo proxychains4 wget https://archive.kali.org/archive-keyring.gpg -O "${archive_keyring}/kali-archive-keyring.gpg"
+
+#3,
+echo -e '\n\n'
+echo -e '!!!WARNING!!!! have to manually create and add the next line to /etc/apt/sources.list.d/kali.list: """'
+echo -e 'deb [signed-by=/usr/share/keyrings/kali-archive-keyring.gpg] https://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware\n"""'
+
+echo -e '\n(4/8) done. \n'
