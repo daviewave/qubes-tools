303467a48ddff95a13e61065eb06029ce54034c3
ds <dav.silveira@proton.me>
2025-06-05 20:04:43 -0400

    added to alias file

--- ali.sh
+++ ali.sh
@@ -20,3 +20,16 @@ alias qp="qvm-prefs"
 alias qup="qubes-prefs"
 alias qd="qvm-shutdown"
 alias qrm="qvm-remove"
+#-- lsd --#
+alias lsd="sudo lsd -A --icon never"
+alias lsds="sudo lsd -A --icon never -Z"
+alias ez="sudo lsd -A --icon never --tree"
+alias ezz="sudo lsd -A --icon never --tree --depth"
+alias ezs="sudo lsd -A --icon never --tree -Z"
+alias ezzs="sudo lsd -A --icon never --tree -Z --depth"
+
+#-- micro --#
+alias m="micro"
+
+
+alias sctl="sudo systemctl"
